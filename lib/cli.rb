52314afae7e7b59dd1a9825660a5ac09120f6502
Ryan Erricson <ryanerricson@gmail.com>
2021-02-25 12:25:07 -0500

    Cleaned up spacing

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,9 @@ require 'colorize'
 
 class ShowFinder::CLI 
 
+
+## This is the flow of the CLI that calls the methods in correct order
+
 def run
     introduction
     input = ""
@@ -10,13 +13,16 @@ def run
     check_input
 end
 
+## This is the standard intro of the CLI
 
 def introduction
     puts "--------------------------------------------------------------------"
     puts "Hello you look like you have some free time on your hands..."
     puts "Might I suggest a new show?"
 end
-###This is where I would say yes shows the random list of 15 TV Shows
+
+
+###This is where the CLI shows the random list of 15 TV Shows
 def list_shows
 
     @show_list = ShowFinder::Show.all.each.with_index(1) do |show, index|
@@ -30,6 +36,13 @@ def list_shows
     puts "-----------------------------------------------------------------------"
     
 end
+
+# This validates user input and makes decisions based upon them
+# If user input is an integer that is between 1-20 it will be accepted and show program details, a method defined in the show class
+# The user is then looped back into the check input method of the CLI
+# If the user input is list, the list of shows is displayed again
+# If the user input is end or exit, the program is ended
+# If the user input is anything else, it will prompt the user to use a valid input
 
 def check_input
     input = gets.strip
@@ -54,32 +67,4 @@ def check_input
     end
 
 end
-
-
-
-
-        
-### From here there would be an indexed number of shows (would like to have it be the arrow that selects in the future)
-
-### Choose any of the shows in order for you to see details about that specific show
-
-####Show Details: #{index}. #{show.name}
-### genres: summary: premiered:
-
-### And then from there it would loop you back to that list of shows where you would be able to grab something and show the details again.
-
-### Also would like to employ a function where you could display all of that info that could individually select by tapping alll
-
-### Would be cool to be able to generate a new list of shows
-
-
-##Issues that might have to be worked around below
-### generating a list of only 15 objects from my API, I think a loop with a counter might work for that where it does pull all of the shows from that list, but it only saves continued
-## 15 of them and then by using an @@all and .all method I can access that list and make a print method that would go ahead and print that list from the @@all array
-### going to have to create some sort of play function with an end function that could be selected at various points when prompted
-###CLI prompt going to need to respond to prompts from user selecting shows by index or name value at first and then trying to make it that arrow menu
-###Can use gsub method to remove HTML from the summaries <p> <b>
-###Genres are in an array, going to have to use join(", ") to make them present as wanted
-
-
 end
